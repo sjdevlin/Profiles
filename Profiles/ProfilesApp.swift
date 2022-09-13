72ed11f4f2a9ab39e0528e0d324c1d523f3070a9
Stephen Devlin <sjdevlin@gmail.com>
2022-09-13 13:58:48 +0100

    Initial Commit

---
+++ Profiles/ProfilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfilesApp.swift
+//  Profiles
+//
+//  Created by Stephen Devlin on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProfilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
