21799d2695ccc8a6bb1433443cf0df659b2ed5c3
sjdevlin <sjdevlin@gmail.com>
2023-06-07 10:21:20 +0100

    This is last working version that uses the motion tracker.

--- Profiles/Views/ContentView.swift
+++ Profiles/Views/ContentView.swift
@@ -66,8 +66,12 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
+
+    static let appStateForPreview = AppState()
+
     static var previews: some View {
         ContentView()
+            .environmentObject(appStateForPreview)
             .previewInterfaceOrientation(.portraitUpsideDown)
             .preferredColorScheme(.dark)
     }
